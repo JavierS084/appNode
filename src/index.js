a9dc66c52214c4780dd99300147579e03f07f948
JavierS084 <javier.sanchezb084@gmail.com>
2021-11-26 11:55:00 -0300

    v0.0.3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require ('express');
 const app = express();
 const path = require('path');
-const exphbs = require('expess-handlebars');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const  session = require('express-session');
 //settings
 
 app.set('port', process.env.PORT || 3000)
@@ -10,19 +12,34 @@ app.set('views', path.join(__dirname, 'wiews'));
 
 app.engine('.hbs', exphbs ({
 
-
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialDir: path.join(app.get('views', 'partials')),
     exrname: '.hbs'
-}));
-//middlewares
 
+}));
+
+app.search('view engine', 'hbs');
+
+
+//Middlewares
+
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.use(session({
+    secret: 'mysecretapp',
+    resave: true,
+    saveUninitialized: true
+}));
 
 //global variables
 
 
 // routes
+app.use(require('.routes/index'));
+app.use(require('./routes/users'))
+app.use(require('./routes/notes'))
+
 
 
 //static files
