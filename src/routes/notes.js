a988b5299650a51643a782991baeaf0e3245c5b4
JavierS084 <javier.sanchezb084@gmail.com>
2021-11-29 11:58:26 -0300

    V0.0.9

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,17 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const Note = require('../models/Note');
 
 router.get('/notes/add', (req, res) => {
     res.render('notes/new-note');
-})
+});
 
-/***
- * 
- router.get('/notes', (req, res) => {
-     res.render('');
- });
- * 
- */
+router.post('/notes/new-note', (req, res) => {
+    const { title, description } = req.body;
+    const errors = [];
+    if(!title) {
+        errors.push({text: 'Please Write a Title'});
+    }
+
+
+    if(!description) {
+        errors.push({text: 'Please Write a Description'});
+
+    }
+
+    if(errors.length > 0) {
+        res.render('notes/new-note', {
+            errors,
+            title,
+            description
+
+        });
+    } else {
+        const newNote = new Note({ title, description});
+        console.log(newNote);
+        res.send('Ok');
+    }
+    
+   
+});
+
+router.get('/notes', (req, res) => {
+    res.render('');
+});
 
 
 module.exports = router;
