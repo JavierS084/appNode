876ff6268ae7de62083357c65ead259dc308e968
JavierS084 <javier.sanchezb084@gmail.com>
2021-12-18 12:21:47 -0300

    V3.5

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require ('express');
 const path = require('path');
-
+const Handlebars = require('handlebars')
 const { engine } = require('express-handlebars');
 
 const methodOverride = require('method-override');
 const  session = require('express-session');
 const flash = require ('connect-flash');
-const passport = require('passport')
+const passport = require('passport');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
 
 
 
@@ -29,7 +30,8 @@ app.engine('.hbs', engine ({
     defaultLayout: 'main',
     extname: '.hbs',
     layoutsDir: path.join(app.get('views'), 'layouts'),
-    partialsDir: path.join(app.get('views'), 'partials')
+    partialsDir: path.join(app.get('views'), 'partials'),
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
     
  
  }));
@@ -59,6 +61,7 @@ app.use((req, res, next) => {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
     next();
 })
 
